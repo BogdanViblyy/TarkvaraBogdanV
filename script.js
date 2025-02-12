f6a1bc57a69130e816a0cec1e0c9dc9b3dae004f
Daria Halchenko <daragalcenko3@gmail.com>
2025-02-12 13:33:37 +0200

    Tee nii, et iga nupuvajutus muudaks taustavärvi juhuslikuks.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.getElementById("muudaVarvi").addEventListener("click", function() {
     // Kirjuta siia kood, mis muudab taustavärvi juhuslikuks värviks
+    const randomvarv = "#" + Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = randomvarv;
 });
